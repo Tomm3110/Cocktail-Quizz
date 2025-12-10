a00bdcbcb1b148d45f4f403c7dade750122ecd20
Tom <tom_firmin@ens.unvi-artois.fr>
2025-12-10 18:12:32 +0100

    adding view login & register and route

--- app/app.py
+++ app/app.py
@@ -75,6 +75,14 @@ def scores():
 
     return render_template('scores.html', scores=scores)
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
